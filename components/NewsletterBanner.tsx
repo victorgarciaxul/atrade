24b859f540121af92cecab9b5242ac8cbd1e1100
VictorGarRos <vrdesign87@gmail.com>
2026-06-26 12:56:09 +0200

    Match newsletter section bg to iframe white background

--- components/NewsletterBanner.tsx
+++ components/NewsletterBanner.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 export default function NewsletterBanner() {
   return (
     <>
-      <section className="bg-gray-50 border-t border-gray-200 py-10">
+      <section className="bg-white border-t border-gray-100 py-10">
         <div className="max-w-[1512px] mx-auto px-6 flex flex-col md:flex-row items-center justify-between gap-6">
           <div>
             <p className="text-primary font-brand font-[600] text-lg">Suscríbete a nuestra newsletter</p>
