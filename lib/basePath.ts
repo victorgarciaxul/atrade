ffd89ed2424ba7834ac3845893dfeddf3aa5b8af
VictorGarRos <vrdesign87@gmail.com>
2026-07-29 20:34:43 +0200

    Revert "Arreglar imágenes rotas bajo basePath: loader personalizado en vez de /_next/image"
    This reverts commit fd78c8d0caf6a2effa074d6300ef7f7e65687d5d.

--- lib/basePath.ts
+++
@@ -1,5 +0,0 @@
-/**
- * basePath de la app (ver next.config.ts). Se centraliza aquí para que
- * next.config.ts y lib/imageLoader.ts usen siempre el mismo valor.
- */
-export const BASE_PATH = "/impulsa";
